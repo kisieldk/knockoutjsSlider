06468d524bcea32501893900ca68f00868006671
Damian Kisielewski <namian.Kisielewski@dfc.local>
2017-02-13 11:27:30 +0100

    Initial sample jQuery-UI custom slider with knockoutjs

---
+++ AppViewModel.js
@@ -0,0 +1,70 @@
+﻿ko.bindingHandlers.jqSliderLoanAmount = {
+	init: function (element, valueAccessor, allBindingsAccessor) {
+		//initialize the control
+		var options = allBindingsAccessor().jqOptions || {};
+		$(element).slider(options).slider("pips").slider("float", {suffix:"k$"});
+
+		//handle the value changing in the UI
+		ko.utils.registerEventHandler(element, "slidechange", function () {
+			//would need to do some more work here, if you want to bind against non-observables
+			var observable = valueAccessor();
+			observable($(element).slider("value"));
+		});
+
+	},
+	//handle the model value changing
+	update: function (element, valueAccessor) {
+		var value = ko.utils.unwrapObservable(valueAccessor());
+		$(element).slider("value", value);
+
+	}
+
+
+};
+ko.bindingHandlers.jqSliderPeriodValue = {
+	init: function (element, valueAccessor, allBindingsAccessor) {
+		//initialize the control
+		var options = allBindingsAccessor().jqOptions || {};
+		$(element).slider(options).slider("pips").slider("float");
+
+		//handle the value changing in the UI
+		ko.utils.registerEventHandler(element, "slidechange", function () {
+			//would need to do some more work here, if you want to bind against non-observables
+			var observable = valueAccessor();
+			observable($(element).slider("value"));
+		});
+
+	},
+	//handle the model value changing
+	update: function (element, valueAccessor) {
+		var value = ko.utils.unwrapObservable(valueAccessor());
+		$(element).slider("value", value);
+
+	}
+
+
+};
+
+var viewModel = {
+	loanAmount: ko.observable(300),
+	periodValue: ko.observable(15),
+	instalment: function () {
+		var month = this.periodValue() * 12;
+		var intrestRate = 0.0259;
+		var capitalInstallment = this.loanAmount() *1000 / month;
+		var intrestInstalment = (this.loanAmount()*1000 * intrestRate) / 360 * 30;
+		var installmentValue = capitalInstallment + intrestInstalment;
+		return roundNumber(installmentValue,2);
+	}
+};
+
+ko.applyBindings(viewModel);
+
+function roundNumber(num, scale) {
+	var number = Math.round(num * Math.pow(10, scale)) / Math.pow(10, scale);
+	if (num - number > 0) {
+		return (number + Math.floor(2 * Math.round((num - number) * Math.pow(10, (scale + 1))) / 10) / Math.pow(10, scale));
+	} else {
+		return number;
+	}
+};
